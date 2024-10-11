7590ebcbefdf7c226ff03eb4489664d1b5ea689c
jtian9 <jtian9@usfca.edu>
2024-10-10 19:31:10 -0700

    created basis for getting history

--- src/main/java/com/example/springtest/controller/LearnerController.java
+++ src/main/java/com/example/springtest/controller/LearnerController.java
@@ -1,30 +1,41 @@
 package com.example.springtest.controller;
 
 import com.example.springtest.entity.Learner;
+import com.example.springtest.entity.SearchHistory;
 import com.example.springtest.repository.LearnerRepository;
+import com.example.springtest.repository.SearchHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequestMapping("/api/learner")
 @RestController
 public class LearnerController {
 
     private final LearnerRepository learnerRepository;
+    private final SearchHistoryRepository searchHistoryRepository;
 
     @Autowired
-    public LearnerController(LearnerRepository learnerRepository) {
+    public LearnerController(LearnerRepository learnerRepository, SearchHistoryRepository searchHistoryRepository) {
         this.learnerRepository = learnerRepository;
+        this.searchHistoryRepository = searchHistoryRepository;
     }
 
-    @GetMapping("/learners")
+    @GetMapping("/all")
     public Iterable<Learner> findAllLearners() {
         return this.learnerRepository.findAll();
     }
 
-    @PostMapping("/learners")
+    @PostMapping("/add")
     public Learner addLearner(@RequestBody Learner learner) {
         return this.learnerRepository.save(learner);
+    }
+
+    @GetMapping("/getHistory")
+    public List<SearchHistory> getUsersByUsername(@RequestParam String username) {
+        List<SearchHistory> histories = searchHistoryRepository.findByLearnerUsername(username);
+        return histories;
     }
 }
--- src/main/java/com/example/springtest/repository/SearchHistoryRepository.java
+++ src/main/java/com/example/springtest/repository/SearchHistoryRepository.java
@@ -4,5 +4,8 @@ import com.example.springtest.entity.Learner;
 import com.example.springtest.entity.SearchHistory;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SearchHistoryRepository extends CrudRepository<SearchHistory, Integer> {
+    List<SearchHistory> findByLearnerUsername(String username);
 }
